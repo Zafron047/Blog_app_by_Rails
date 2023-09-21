b8f4fb94b5949ce860ccdf119985d1c841153f77
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-09-21 21:20:05 +0600

    Add tests for counter validation in User Model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,4 +12,22 @@ RSpec.describe User, type: :model do
       expect(subject).to_not be_valid
     end
   end
+
+  describe 'User Model counter validation tests' do
+    it 'posts_counter must be an integer otherwise' do
+      subject.posts_counter = 'You are not an integer'
+      expect(subject).to_not be_valid
+    end
+
+    it 'posts_counter should be greater than or equal to zero' do
+      subject.posts_counter = -2
+      expect(subject).to_not be_valid
+
+      subject.posts_counter = 0
+      expect(subject).to be_valid
+      
+      subject.posts_counter = 2
+      expect(subject).to be_valid
+    end
+  end
 end
