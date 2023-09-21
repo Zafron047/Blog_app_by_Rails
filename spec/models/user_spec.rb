5520042d6041121ed5c00e3e1e827371d4f99ebe
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-09-21 21:29:15 +0600

    Add test for method in comment model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  subject { User.new(name: 'Tom') }
+
   describe 'User Model Name validation tests' do
     it 'should have name present' do
       subject.name = 'Tom'
