51e526fa4b1fa198c44aa7e152a389f30d489e98
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-09-21 21:18:22 +0600

    Add test for Name validation in User Model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'User Model Name validation tests' do
+    it 'should have name present' do
+      subject.name = 'Tom'
+      expect(subject).to be_valid
+    end
+
+    it 'should not have name field empty, if then' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+  end
 end
