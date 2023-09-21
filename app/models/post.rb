318d0d09946b2cc274171759b73f12b7ef6c37e2
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-09-21 14:25:01 +0600

    Create method in post class to display comments order by created date and limit by 5

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,4 +15,8 @@ class Post < ApplicationRecord
   def update_posts_counter
     author.update(posts_counter: author.posts.count)
   end
+
+  def five_most_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
