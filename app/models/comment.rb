c4bf9877d20ea4a95cceef2abccff7bc15afaece
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-09-21 14:13:02 +0600

    Create model for Comment class

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,10 @@
 class Comment < ApplicationRecord
+  belongs_to :author, class_name: 'User', foreign_key: :user_id
+  belongs_to :post, class_name: 'Post', foreign_key: :post_id
+
+  attribute :text, :text
+  
+  after_create :update_posts_counter
+  after_destroy :update_posts_counter
+  after_delete :update_posts_counter
 end
