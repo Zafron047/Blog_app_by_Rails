314735fa8469d9f0f4da6ac7f04ab1053ac5ed62
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-09-21 14:12:25 +0600

    Create model for Post class

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+  has_many :comments
+  has_many :likes
+  belongs_to :author, class_name: 'User'
+
+  attribute :title, :string
+  attribute :text, :text
+  attribute :comments_counter, :integer, default: 0
+  attribute :likes_counter, :integer, default: 0
+
+  after_create :update_posts_counter
+  after_destroy :update_posts_counter
+  after_delete :update_posts_counter
 end
