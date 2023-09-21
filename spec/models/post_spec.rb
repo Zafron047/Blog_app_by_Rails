e6585b7cd4f33fc99b8ac4a6df735b33af647c95
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-09-21 21:22:48 +0600

    Add tests to validate title in Post Model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { Post.new(title: 'Title', author: User.create(name: 'Dick')) }
+
+  describe 'Post Model title validation tests' do
+    it 'should have title present' do
+      subject.title = 'Title'
+      expect(subject).to be_valid
+    end
+
+    it 'should not have title field empty, if then' do
+      subject.title = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'title should be less than or equal to 250 characters' do
+      subject.title = 'a' * 251
+      expect(subject).to_not be_valid
+
+      subject.title = 'a' * 250
+      expect(subject).to be_valid
+    end
+  end
+
 end
