5b4d926b8bfe4eccf29f917f5faa20c676f6271c
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-09-25 20:07:27 +0600

    Add http and render tests for index show file

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -17,4 +17,21 @@ RSpec.describe 'Users', type: :request do
       expect(response.body).to include('This is the index file for testing')
     end
   end
+
+  describe 'GET /show' do
+    it 'returns http success' do
+      get user_path(1)
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'returns http success' do
+      get user_path(1)
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes correct placeholder text in the response body' do
+      get user_path(1)
+      expect(response.body).to include('This is the show user section for testing')
+    end
+  end
 end
