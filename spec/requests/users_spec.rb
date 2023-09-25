cdd8665efeb9be7b4a6e5d026b4a0baf885e20c2
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-09-25 19:57:31 +0600

    Add http and render tests for index file

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,6 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'returns http success' do
+      get users_path
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'returns http success' do
+      get users_path
+      expect(response).to render_template(:index)
+    end
+
+    it 'includes correct placeholder text in the response body' do
+      get users_path
+      expect(response.body).to include('This is the index file for testing')
+    end
   end
 end
