8c936d5eda2cb342cfc148534d1f6b28293a6c4f
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-09-27 14:49:45 +0600

    Create Index and show pages for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,12 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
+  end
 
-  def show; end
+  def show
+    @posts = Post.all
+    @post = Post.find(params[:id])
+    @recent_comments = @post.five_most_recent_comments
+  end
 end
