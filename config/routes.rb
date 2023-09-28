51aeeded801a0d9287642f669b84f96c6da33688
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-09-28 16:06:46 +0600

    Add functionalities to submit new post via form

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:index, :new, :create]
     end
   end
-
-  # resources :posts, only: [:index, :show]
   root 'users#index'
 end
