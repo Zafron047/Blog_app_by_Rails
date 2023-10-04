936c845ecee4480c42404b0028f80e18b9f1e937
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-10-04 15:36:53 +0600

    Setup Devise and migrate db

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:index, :new, :create]
       resources :likes, only: [:create]
     end
   end
-  root 'users#index'
+  # root 'users#index'
 end
