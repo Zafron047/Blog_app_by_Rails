0cdd66c53b4b2b7c516b3866a96b922a36512773
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-10-05 16:42:57 +0600

    Add role admin to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   has_many :posts, foreign_key: :author_id
@@ -11,6 +9,7 @@ class User < ApplicationRecord
   attribute :photo, :string
   attribute :bio, :text
   attribute :posts_counter, :integer, default: 0
+  enum role: %i[user admin]
 
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
