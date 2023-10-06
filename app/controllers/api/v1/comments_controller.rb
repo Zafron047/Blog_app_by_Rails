92a35c9798b462b57d66d3aafc5334ccc04c4d08
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-10-06 16:56:58 +0600

    Fix routes and linters

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -32,7 +32,7 @@ class Api::V1::CommentsController < ApplicationController
   end
 
   private
-  
+
   def comment_params
     params.require(:comment).permit(:text, :user_id)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,18 @@ Rails.application.routes.draw do
   end
   
   resources :users, only: [:index, :show] do
-    # resources :posts, only: [:index, :show, :new, :create] do
-    #   resources :comments ,only:[:index,:new,:create]
     resources :posts, only: [:index, :show, :new, :create, :destroy] do
       resources :comments ,only:[:index,:new,:create, :destroy]
       resources :likes ,only:[:create]
+    end
+  end
+  namespace :api do
+    namespace :v1 do
+      resources :users do
+        resources :posts, only: [:index] do
+         resources :comments, only: [:index, :create]
+        end
+      end
     end
   end
 end
